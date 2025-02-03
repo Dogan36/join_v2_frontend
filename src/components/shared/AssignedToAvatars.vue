49f40a4dc6515912b6d54b92991a9728917d00a3
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2025-02-03 14:34:23 +0100

    refactor AssignedToAvatars component; enhance avatar styling with dynamic background color, implement dark/light text color logic, and improve props validation

--- src/components/shared/AssignedToAvatars.vue
+++ src/components/shared/AssignedToAvatars.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="assignedTo">
-    <div class="cardAvatar" v-for="contact in assignedTo">
+    <div
+      class="cardAvatar"
+      :key="contact.avatar"
+      :style="{ background: contact.color.hex_value, color: isDarkBackground(contact.color.hex_value) ? '#fff' : '#000' }"
+      v-for="contact in assignedTo" 
+    >
       {{ contact.avatar }}
+      
     </div>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, computed } from 'vue';
+
+
+// Deine 'assignedTo' Daten, die du von der übergeordneten Komponente erhältst
 const props = defineProps({
-    assignedTo: Array,
-    });
+  assignedTo: {
+    type: Array,
+    required: true
+  }
+});
+
+function hexToRgb(hex) {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    return { r, g, b };
+  }
+
+  // Berechnung, ob der Hintergrund dunkel oder hell ist
+  const isDarkBackground = (hex) => {
+    const rgb = hexToRgb(hex);
+    const yiq = (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
+    return yiq < 128; // Dunkel: Textfarbe weiß, hell: Textfarbe schwarz
+  };
+
+ 
 </script>
 
 <style scoped>
@@ -29,7 +57,7 @@ const props = defineProps({
     gap: 10px;
     width: 35px;
     height: 35px;
-    background: #0190e0;
+    background:rgb(88, 160, 201);
     border: 1px solid #ffffff;
     border-radius: 58px;
     flex: none;
